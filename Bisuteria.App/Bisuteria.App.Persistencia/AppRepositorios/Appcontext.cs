55df71a5ce3a8fce2ca6a78933ed07fee0ba289b
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2021-10-05 18:55:25 -0500

    Persitencia creada (DBContext)

---
+++ Bisuteria.App/Bisuteria.App.Persistencia/AppRepositorios/Appcontext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Bisuteria.App.Dominio.Entidades;
+namespace Bisuteria.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Material> Materiales { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+               .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog =ProyectoG58");
+            }
+        }
+    }
+}
+
