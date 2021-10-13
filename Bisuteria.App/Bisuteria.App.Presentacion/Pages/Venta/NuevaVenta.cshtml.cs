77bd2cc2da99259cb1fffd2ab00b8229a346d0b3
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2021-10-13 02:36:03 -0500

    Nuevas paginas

---
+++ Bisuteria.App/Bisuteria.App.Presentacion/Pages/Venta/NuevaVenta.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bisuteria.App.Presentacion
+{
+    public class NuevaVentaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
